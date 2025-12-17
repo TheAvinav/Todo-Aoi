43e84ffaf47fb2362e1899589420d9bb010bcac1
TheAvinav <avinav.mendu890@gmail.com>
2025-12-17 13:44:47 +0530

    changing todo-task display

--- todo.js
+++ todo.js
@@ -1,4 +1,3 @@
-
 document.addEventListener('DOMContentLoaded', () => {
     const taskInput = document.getElementById('task-input');
     const addTaskBtn = document.getElementById('add-task-btn');
