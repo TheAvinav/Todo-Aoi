a39ae4aa8fd465d705d97fd580907aca40550da3
TheAvinav <avinav.mendu890@gmail.com>
2025-12-22 22:17:09 +0530

    added image, better version with edit and delete

--- todo.js
+++ todo.js
@@ -3,9 +3,11 @@ document.addEventListener('DOMContentLoaded', () => {
     const addTaskBtn = document.getElementById('add-task-btn');
     const taskList = document.getElementById('task-list');
     const emptyImage = document.querySelector('.empty-image');
+    const todosContainer = document.querySelector('todos-container');
 
     const toggleEmptyState = () => {
         emptyImage.style.display = taskList.children.length === 0 ? 'block' : 'none';
+        todosContainer.style.width = taskList.children.length > 0 ? '100%' : '50%';
     };
 
     const addTask = (event) => {
@@ -17,14 +19,36 @@ document.addEventListener('DOMContentLoaded', () => {
         }
 
         const li = document.createElement('li');
+
         li.innerHTML = `
         <input type="checkbox" class="checkbox">
         <span>${taskText}</span>
+        <div class="task-buttons">
+            <button class="edit-button"><i class="fa-solid fa-pen"></i></button>
+            <button class="delete-button"><i class="fa-solid fa-trash"></i></button>
+        </div>
         `;
+
+        const checkbox = li.querySelector('.checkbox');
+        const editButton = li.querySelector('.edit-button');
+
+        
+        editButton.addEventListener('click', () => {
+            if (!checkbox.checked) {
+                taskInput.value = li.querySelector('span').textContent;
+                li.remove();
+                toggleEmptyState();
+            }
+        })
+
+        li.querySelector('.delete-button').addEventListener('click', () => {
+            li.remove();
+            toggleEmptyState();
+        });
+        
         taskList.appendChild(li);
 
         // Add checkbox click event
-        const checkbox = li.querySelector('.checkbox');
         checkbox.addEventListener('change', () => {
             li.classList.toggle('checked');
         });
